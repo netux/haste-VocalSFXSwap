040059f8e823eb1508eeb2ac75b49338f60d1659
Martín (Netux) Rodriguez <send.email@netux.site>
2025-11-12 08:31:41 -0300

    feat: add parsing of .hastevocalssfx.json files, add ability to define custom clip file paths, and per SFX volume multiplier settings
    Now, individual audio files are added only if there is no config for them.

--- HasteVocalSfxSwapMod/VocalSfxSwapMod.cs
+++ HasteVocalSfxSwapMod/VocalSfxSwapMod.cs
@@ -1,5 +1,6 @@
 ﻿using Landfall.Haste;
 using Landfall.Modding;
+using Newtonsoft.Json;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -10,9 +11,11 @@ namespace HasteVocalSfxSwapMod;
 [LandfallPlugin]
 public class VocalSfxSwapMod
 {
-    private static readonly FieldInfo[] vocalBankSfxInstanceFields = typeof(VocalBank).GetFields().Where((field) => field.FieldType == typeof(SFX_Instance)).ToArray();
+    private static readonly FieldInfo[] vocalBankSfxInstanceFields = typeof(VocalBank).GetFields()
+        .Where((field) => field.FieldType == typeof(SFX_Instance))
+        .ToArray();
 
-    protected static Dictionary<int, Dictionary<string, List<string>>> vocalBanksReplacements = [];
+    protected static Dictionary<int, Dictionary<string, SfxInstanceSwapConfig>> vocalBanksReplacements = [];
     protected static Dictionary<int, VocalBank> skinVocalBankCache = [];
 
     private static VocalBank? baseZoeVocalBank;
@@ -46,16 +49,18 @@ public class VocalSfxSwapMod
 
     private static string VocalBankFieldToSfxName(FieldInfo field)
     {
-        return field.Name.Replace("Vocals", "");
+        return field.Name.Remove(field.Name.IndexOf("Vocals"), "Vocals".Length);
     }
 
     private static void LoadVocalSfxs(string directory)
     {
+        Dictionary<int, Dictionary<string, List<string>>> foundSoundFilesPerSkinPerSfx = [];
+
         void TryLoadWavFile(string wavFilePath)
         {
             // (name).(skin index).(sfx name).wav
             // (name).(skin index).(sfx name).(number).wav
-            var split = wavFilePath.Split(".");
+            var split = Path.GetFileName(wavFilePath).Split(".");
 
             if (split.Length < 4 || split.Length > 5)
             {
@@ -67,22 +72,23 @@ public class VocalSfxSwapMod
                 return;
             }
 
-            var sfxName = split[2];
+            var sfxNameInFileName = split[2];
 
             Dictionary<string, List<string>> replacements;
-            if (!vocalBanksReplacements.ContainsKey(skinIndex))
+            if (!foundSoundFilesPerSkinPerSfx.ContainsKey(skinIndex))
             {
                 replacements = [];
-                vocalBanksReplacements.Add(skinIndex, replacements);
+                foundSoundFilesPerSkinPerSfx.Add(skinIndex, replacements);
             }
             else
             {
-                replacements = vocalBanksReplacements[skinIndex];
+                replacements = foundSoundFilesPerSkinPerSfx[skinIndex];
             }
 
             foreach (var field in vocalBankSfxInstanceFields)
             {
-                if (sfxName.ToLower() == VocalBankFieldToSfxName(field).ToLower())
+                var sfxName = VocalBankFieldToSfxName(field);
+                if (sfxNameInFileName.ToLower() == sfxName.ToLower())
                 {
                     List<string> clipPaths;
                     if (replacements.ContainsKey(field.Name))
@@ -92,14 +98,67 @@ public class VocalSfxSwapMod
                     else
                     {
                         clipPaths = [];
-                        replacements[field.Name] = clipPaths;
+                        replacements[sfxName] = clipPaths;
                     }
 
                     clipPaths.Add(wavFilePath);
+                    break;
                 }
             }
+        }
 
-            Debug.Log($"[{nameof(VocalSfxSwapMod)}] Registered {sfxName} for skin {skinIndex}: {wavFilePath}");
+        void TryLoadConfigFile(string configFilePath)
+        {
+            // (name).(skin index).hastevocalssfx.json
+            var split = Path.GetFileName(configFilePath).Split(".");
+
+            if (split.Length != 4)
+            {
+                return;
+            }
+
+            if (!int.TryParse(split[1], out int skinIndex))
+            {
+                return;
+            }
+
+            using StreamReader file = File.OpenText(configFilePath);
+
+            try
+            {
+                Dictionary<string, SfxInstanceSwapConfig>? configs = new JsonSerializer()
+                    .Deserialize<Dictionary<string, SfxInstanceSwapConfig>>(new JsonTextReader(file));
+                if (configs == null)
+                {
+                    // Not sure when this can happen...
+                    return;
+                }
+
+                var configDirectoryBasePath = Path.GetDirectoryName(configFilePath);
+
+                foreach (var config in configs.Values)
+                {
+                    if (config.Clips == null)
+                    {
+                        continue;
+                    }
+
+                    // Post-process: Make all paths absolute
+                    config.Clips = config.Clips
+                        .Select(soundFilePath => Path.IsPathFullyQualified(soundFilePath)
+                            ? soundFilePath
+                            : Path.Combine(configDirectoryBasePath, soundFilePath)
+                        )
+                        .ToArray();
+                }
+
+                vocalBanksReplacements.Add(skinIndex, configs);
+                Debug.Log($"[{nameof(VocalSfxSwapMod)}] Loaded vocal sfx config file for skin {skinIndex}: {configFilePath}");
+            }
+            catch (JsonException error)
+            {
+                Debug.LogError($"[{nameof(VocalSfxSwapMod)}] Could not parse Vocal SFX configuration: {error}");
+            }
         }
 
         foreach (var filePath in Directory.GetFiles(directory))
@@ -107,6 +166,50 @@ public class VocalSfxSwapMod
             if (filePath.EndsWith(".wav"))
             {
                 TryLoadWavFile(filePath);
+            }
+            else if (filePath.EndsWith(".hastevocalssfx.json"))
+            {
+                TryLoadConfigFile(filePath);
+            }
+        }
+
+        foreach (var skinIndex in foundSoundFilesPerSkinPerSfx.Keys)
+        {
+            Dictionary<string, SfxInstanceSwapConfig> skinConfigs;
+            if (vocalBanksReplacements.ContainsKey(skinIndex))
+            {
+                skinConfigs = vocalBanksReplacements[skinIndex];
+            }
+            else
+            {
+                skinConfigs = [];
+                vocalBanksReplacements.Add(skinIndex, skinConfigs);
+            }
+
+            foreach (var sfxName in foundSoundFilesPerSkinPerSfx[skinIndex].Keys)
+            {
+                SfxInstanceSwapConfig config;
+                if (skinConfigs.ContainsKey(sfxName))
+                {
+                    config = skinConfigs[sfxName];
+                    if (config.Clips != null)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    config = new();
+                    skinConfigs[sfxName] = config;
+                }
+
+                config.Clips = foundSoundFilesPerSkinPerSfx[skinIndex][sfxName].ToArray();
+                Debug.Log($"[{nameof(VocalSfxSwapMod)}] Adding individual vocal sfx sound files for sfx {sfxName}, skin {skinIndex}:\n{
+                    string.Join("\n",
+                        foundSoundFilesPerSkinPerSfx[skinIndex][sfxName]
+                            .Select((filePath) => $"- {filePath}")
+                    )
+                }");
             }
         }
     }
@@ -118,7 +221,7 @@ public class VocalSfxSwapMod
 
         if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.DataProcessingError)
         {
-            Debug.Log($"[{nameof(VocalSfxSwapMod)}] could not load audio clip at \"{path}\": {uwr.error}");
+            Debug.Log($"[{nameof(VocalSfxSwapMod)}] Could not load audio clip at \"{path}\": {uwr.error}");
             return null;
         }
 
@@ -129,7 +232,7 @@ public class VocalSfxSwapMod
     {
         if (PlayerVocalSFX.Instance == null)
         {
-            Debug.LogError($"[{nameof(VocalSfxSwapMod)}] Could not replace vocals for skin {skinIndex}: could not find PlayerVocalSFX instance");
+            //Debug.LogError($"[{nameof(VocalSfxSwapMod)}] Could not replace vocals for skin {skinIndex}: could not find PlayerVocalSFX instance");
             return;
         }
 
@@ -169,9 +272,10 @@ public class VocalSfxSwapMod
             // Create new SFX_Instances for replacement SFX clips
             foreach (var field in vocalBankSfxInstanceFields)
             {
-                if (!skinReplacements.ContainsKey(field.Name))
+                var sfxName = VocalBankFieldToSfxName(field);
+                if (!skinReplacements.ContainsKey(sfxName))
                 {
-                    //Debug.Log($"[{nameof(VocalSfxSwapMod)}] Skin {skinIndex} has no {field.Name} replacements");
+                    //Debug.Log($"[{nameof(VocalSfxSwapMod)}] Skin {skinIndex} has no {sfxName} replacements");
                     continue;
                 }
 
@@ -184,8 +288,22 @@ public class VocalSfxSwapMod
                 newSfxInstance.settings = oldSfxInstance.settings;
                 newSfxInstance.lastTimePlayed = oldSfxInstance.lastTimePlayed;
 
+                var sfxInstanceConfig = skinReplacements[sfxName];
+
+                if (sfxInstanceConfig.Settings != null)
+                {
+                    newSfxInstance.settings = new SFX_Settings();
+
+                    foreach (var settingField in typeof(SFX_Settings).GetFields())
+                    {
+                        settingField.SetValue(newSfxInstance.settings, settingField.GetValue(oldSfxInstance.settings));
+                    }
+
+                    newSfxInstance.settings.volume *= sfxInstanceConfig.Settings.VolumeMultiplier;
+                }
+
                 List<AudioClip> clips = [];
-                foreach (var path in skinReplacements[field.Name])
+                foreach (var path in sfxInstanceConfig.Clips ?? [])
                 {
                     Debug.Log($"[{nameof(VocalSfxSwapMod)}] Loading AudioClip {path}");
 
@@ -217,4 +335,18 @@ public class VocalSfxSwapMod
     {
         Debug.Log(string.Join("\n", vocalBankSfxInstanceFields.Select((field) => $"- {VocalBankFieldToSfxName(field)}")));
     }
+}
+
+public record SfxInstanceSwapConfig
+{
+    [JsonProperty("clips")]
+    public string[]? Clips;
+    [JsonProperty("settings")]
+    public SfxSettingsSwap? Settings;
+}
+
+public record SfxSettingsSwap
+{
+    [JsonProperty("volumeMultiplier")]
+    public float VolumeMultiplier = 1f;
 }
