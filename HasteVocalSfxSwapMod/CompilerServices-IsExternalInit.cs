ee9e9f0e2d4f2f883ef56c55b1a8d3695c198ded
Martín (Netux) Rodriguez <send.email@netux.site>
2025-11-14 23:36:52 -0300

    chore: add shim for System.Runtime.CompilerServices.IsExternalInit
    Required to define records using reduced syntax, for some reason... ?
    
    The alternative is changing to net5.0+, but that's not possible for us Unity-bounded plebs.

---
+++ HasteVocalSfxSwapMod/CompilerServices-IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿// This is required to quiet down a compilation error, for some reason
+// https://stackoverflow.com/a/64749403
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace System.Runtime.CompilerServices
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+{
+    internal static class IsExternalInit { }
+}
